73044e101f396c3c8de2676544441a57df42dd6f
leonardotor-lm <leonardotor@gmail.com>
2026-07-13 12:39:23 -0300

    Add TaskStatus constant for task states

---
+++ src/domain/TaskStatus.js
@@ -0,0 +1,13 @@
+export const TaskStatus = Object.freeze({
+
+    INBOX: "INBOX",
+
+    PENDING: "PENDING",
+
+    COMPLETED: "COMPLETED",
+
+    ARCHIVED: "ARCHIVED",
+
+    DELETED: "DELETED"
+
+});
