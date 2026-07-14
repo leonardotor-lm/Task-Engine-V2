75568f6a0b76b3446c9ad3ced932816990d2200b
leonardotor-lm <leonardotor@gmail.com>
2026-07-14 08:49:38 -0300

    Modify render method to accept and display tasks
    Updated the render method to accept a tasks parameter and display a list of tasks.

--- src/ui/MainView.js
+++ src/ui/MainView.js
@@ -1,14 +1,17 @@
 export class MainView {
 
-    render() {
-
-        console.log("Render principal");
+    render(tasks = []) {
 
         const app = document.getElementById("app");
 
         app.innerHTML = `
-            <h2>La aplicación se inició correctamente.</h2>
-            <p>Bienvenido a Task Engine V2.</p>
+            <h2>Mis tareas</h2>
+
+            <ul>
+                ${tasks.map(task => `
+                    <li>${task.title}</li>
+                `).join("")}
+            </ul>
         `;
 
     }
