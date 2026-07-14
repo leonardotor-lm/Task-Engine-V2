1c42b8db1a554cb158282412c66ce6fb894a8840
leonardotor-lm <leonardotor@gmail.com>
2026-07-14 09:39:38 -0300

    Add methods to get and update tasks by ID

--- src/core/TaskService.js
+++ src/core/TaskService.js
@@ -14,6 +14,26 @@ export class TaskService {
         return this.repository.getAll();
     }
 
+    getTaskById(id) {
+        return this.repository.getById(id);
+    }
+
+    updateTask(id, data) {
+
+        const task = this.repository.getById(id);
+
+        if (!task) {
+            return null;
+        }
+
+        task.update(data);
+
+        this.repository.update(task);
+
+        return task;
+
+    }
+
     toggleTask(id) {
         return this.repository.toggleComplete(id);
     }
