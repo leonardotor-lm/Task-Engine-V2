182c8d168250b27aa8ad13f8f2e218fe925fa911
leonardotor-lm <leonardotor@gmail.com>
2026-07-14 08:42:54 -0300

    Implement TaskRepository with CRUD operations

--- src/infrastructure/TaskRepository.js
+++ src/infrastructure/TaskRepository.js
@@ -1,1 +1,43 @@
+import { Task } from "../domain/Task.js";
 
+export class TaskRepository {
+
+    constructor() {
+        this.tasks = [];
+    }
+
+    getAll() {
+        return [...this.tasks];
+    }
+
+    getById(id) {
+        return this.tasks.find(task => task.id === id) ?? null;
+    }
+
+    add(taskData) {
+
+        const task = new Task(taskData);
+
+        this.tasks.push(task);
+
+        return task;
+    }
+
+    update(task) {
+
+        const index = this.tasks.findIndex(t => t.id === task.id);
+
+        if (index === -1) {
+            throw new Error("La tarea no existe.");
+        }
+
+        this.tasks[index] = task;
+    }
+
+    remove(id) {
+
+        this.tasks = this.tasks.filter(task => task.id !== id);
+
+    }
+
+}
