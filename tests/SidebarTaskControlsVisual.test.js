4bd018adfedf6ef4f4134e3817e72257c7fa51f7
leonardotor-lm <leonardotor@gmail.com>
2026-07-30 00:47:43 -0300

    Group primary task controls: tests/SidebarTaskControlsVisual.test.js

--- tests/SidebarTaskControlsVisual.test.js
+++ tests/SidebarTaskControlsVisual.test.js
@@ -40,13 +40,38 @@ test("los filtros rápidos agrupan filtros y orden", () => {
     );
 
     assert.ok(
+        html.indexOf("toggleBulkMode") <
+        html.indexOf("openTaskTools")
+    );
+
+    assert.ok(
+        html.indexOf("openTaskTools") <
+        html.indexOf("toggleCompletedTasks")
+    );
+
+    assert.ok(
         html.indexOf("taskToolsDialog") <
         html.indexOf("toggleCompletedTasks")
+    );
+
+    assert.match(
+        html,
+        /id="toggleBulkMode"[\s\S]*?class="taskToolsButton/
+    );
+
+    assert.match(
+        html,
+        /id="toggleCompletedTasks"[\s\S]*?class="taskToolsButton/
     );
 
     assert.doesNotMatch(
         html,
         /id="toggleTaskMetadata"/
+    );
+
+    assert.match(
+        html,
+        /type="submit"\s+form="taskFilterForm"[\s\S]*?>\s*Aplicar/
     );
 
 });
