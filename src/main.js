d508576c5b895d8a36c7c0fda521d799d5e3471b
leonardotor-lm <leonardotor@gmail.com>
2026-08-05 20:30:33 -0300

    Initialize attachment workflow

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { App } from "./core/App.js";
+import {
+    AttachmentController
+} from "./ui/AttachmentController.js";
 
 const app = new App();
+const attachmentController =
+    new AttachmentController(app);
 
+attachmentController.start();
 app.start();
