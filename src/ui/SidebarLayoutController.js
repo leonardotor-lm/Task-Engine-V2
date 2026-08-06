b855cc9522c5f58e3c02eb4aa8bc21235050fe8b
leonardotor-lm <leonardotor@gmail.com>
2026-08-06 09:42:52 -0300

    Refine planning sidebar order

---
+++ src/ui/SidebarLayoutController.js
@@ -0,0 +1,39 @@
+import {
+    TaskToolbarLayoutController
+} from "./TaskToolbarLayoutController.js";
+
+export class SidebarLayoutController
+    extends TaskToolbarLayoutController {
+
+    ensurePlanningGroup(navigation) {
+
+        const group = super.ensurePlanningGroup(
+            navigation
+        );
+
+        const body = group?.querySelector(
+            ":scope > .sidebarNavigationGroupBody"
+        );
+
+        if (!body) return group;
+
+        [
+            "showAll",
+            "showWaiting",
+            "showCalendar",
+            "showGoals"
+        ].forEach(id => {
+
+            const button = document.getElementById(id);
+
+            if (button) {
+                body.append(button);
+            }
+
+        });
+
+        return group;
+
+    }
+
+}
