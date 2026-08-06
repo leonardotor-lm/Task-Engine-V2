842020f121c11aaf3d5dbdee0754c9528a9d0041
leonardotor-lm <leonardotor@gmail.com>
2026-08-06 08:54:01 -0300

    Initialize contextual task toolbar

--- src/main.js
+++ src/main.js
@@ -8,14 +8,20 @@ import {
 import {
     WaitingController
 } from "./ui/WaitingController.js";
+import {
+    TaskToolbarController
+} from "./ui/TaskToolbarController.js";
 
 const app = new App();
 const attachmentController =
     new AttachmentController(app);
 const waitingController =
     new WaitingController(app);
+const taskToolbarController =
+    new TaskToolbarController(app);
 
 attachmentController.start();
 bindAttachmentSearchReference(app);
 waitingController.start();
+taskToolbarController.start();
 app.start();
