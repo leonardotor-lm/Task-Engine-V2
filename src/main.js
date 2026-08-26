ed6f0e0ef28092ccbe10c182bf59b263db9da6e7
leonardotor-lm <leonardotor@gmail.com>
2026-08-25 21:46:49 -0300

    Wire calendar reminder controller

--- src/main.js
+++ src/main.js
@@ -152,6 +152,9 @@ import {
 import {
     ThemeController
 } from "./ui/ThemeController.js";
+import {
+    CalendarReminderController
+} from "./ui/CalendarReminderController.js";
 
 const app = new App();
 app.aiPreferences = new AiPreferences();
@@ -277,6 +280,8 @@ const strictAdvancedSearchResultsController =
     new StrictAdvancedSearchResultsController(app);
 const themeController =
     new ThemeController(app);
+const calendarReminderController =
+    new CalendarReminderController(app);
 
 attachmentController.start();
 bindAttachmentSearchReference(app);
@@ -297,7 +302,7 @@ syncOptionalDataBridge.start();
 taskFilterSyncBridge.start();
 syncNavigationPreservationController.start();
 smartSyncReconnectionController.start();
-ongoingSyncReconciliationController
+goingSyncReconciliationController
     .start();
 pwaController.start();
 aiSettingsController.start();
@@ -328,5 +333,6 @@ recurrenceLifecycleController.start();
 businessDayRecurrenceController.start();
 viewTaskSummaryController.start();
 strictAdvancedSearchResultsController.start();
+calendarReminderController.start();
 app.start();
 themeController.start();
