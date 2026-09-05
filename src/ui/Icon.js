3cdbd34ae4d032d83c9831ad07a347761943cc99
leonardotor-lm <leonardotor@gmail.com>
2026-09-05 19:40:16 -0300

    Refine mobile trash and activity controls

--- src/ui/Icon.js
+++ src/ui/Icon.js
@@ -46,6 +46,14 @@ const icons = Object.freeze({
         <path d="m6 6 12 12"></path>
     `,
 
+    trash: `
+        <path d="M3 6h18"></path>
+        <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+        <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"></path>
+        <path d="M10 11v6"></path>
+        <path d="M14 11v6"></path>
+    `,
+
     "chevron-down": `
         <path d="m6 9 6 6 6-6"></path>
     `,
