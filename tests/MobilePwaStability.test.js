3c864c1e4c3a40d998d4f1c4d15d1b784ce2893d
leonardotor-lm <leonardotor@gmail.com>
2026-09-12 15:03:09 -0300

    Corregir captura compartida desde Android

--- tests/MobilePwaStability.test.js
+++ tests/MobilePwaStability.test.js
@@ -21,6 +21,6 @@ test("la PWA precarga las hojas críticas de la interfaz móvil", () => {
 
     assert.match(
         serviceWorker,
-        /CACHE_NAME = `\$\{CACHE_PREFIX\}-v7`/
+        /CACHE_NAME = `\$\{CACHE_PREFIX\}-v8`/
     );
 });
